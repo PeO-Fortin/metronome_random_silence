550b1cc22b270cec179e3132ec6e39bf1e05833d
PeO-Fortin <pierreo.fortin@gmail.com>
2025-08-09 17:01:17 -0400

    Grossir affichage

--- src/Affichage.java
+++ src/Affichage.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.plaf.metal.MetalCheckBoxIcon;
 import java.awt.*;
 import java.awt.event.*;
 
@@ -7,16 +8,22 @@ public class Affichage implements ActionListener {
     private final String MSG_ERR = "Vous devez entrer un nombre positif.";
 
     //Attributs d'instance
+    private JFrame fenetre = new JFrame();
     private JPanel panelHaut = new JPanel(new FlowLayout());
     private JPanel panelMilieu = new JPanel(new FlowLayout());
-    private JFrame fenetre = new JFrame();
+
     private JLabel jLBpm = new JLabel("BPM");
-    private JTextField jTFBpm = new JTextField(5);
+    private JTextField jTFBpm = new JTextField("0",3);
+
     private JLabel jLSilence = new JLabel("Nombre de BPM silencieux");
-    private JTextField jTFSilence = new JTextField(5);//8 colonnes
+    private JTextField jTFSilence = new JTextField("0",3);
+
     private JCheckBox jCBRandom = new JCheckBox("Au hasard");
     private JButton jBDemarrer = new JButton("Démarrer");
 
+    private int bpm = 0;
+    private int silence = 0;
+    private boolean random = false;
 
     public Affichage () {
         init();
@@ -24,24 +31,32 @@ public class Affichage implements ActionListener {
 
     public void init() {
         fenetre.setLayout(new BorderLayout());
-        fenetre.setBounds(400, 400, 900, 150);
+        fenetre.setBounds(400, 400, 900, 225);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setTitle("Metronome");
         jCBRandom.setSelected(false);
 
-        jLBpm.setFont(new Font("Arial", Font.BOLD, 20));
+        jLBpm.setFont(new Font("Arial", Font.BOLD, 32));
+        jTFBpm.setFont(new Font("Arial", Font.BOLD, 32));
         panelHaut.add(jLBpm);
         panelHaut.add(jTFBpm);
 
 
-        jLSilence.setFont(new Font("Arial", Font.BOLD, 20));
+        jLSilence.setFont(new Font("Arial", Font.BOLD, 32));
+        jTFSilence.setFont(new Font("Arial", Font.BOLD, 32));
         panelMilieu.add(jLSilence);
         panelMilieu.add(jTFSilence);
-        jCBRandom.setFont(new Font("Arial", Font.BOLD, 20));
+        jCBRandom.setFont(new Font("Arial", Font.BOLD, 32));
+        jCBRandom.setIcon(new MetalCheckBoxIcon() {
+                              @Override
+                              protected int getControlSize() {
+                                  return 32; // Set desired size
+                              }
+                          });
         panelMilieu.add(Box.createHorizontalStrut(20));
         panelMilieu.add(jCBRandom);
 
-        jBDemarrer.setFont(new Font("Arial", Font.BOLD, 20));
+        jBDemarrer.setFont(new Font("Arial", Font.BOLD, 32));
 
         fenetre.add(panelHaut, BorderLayout.NORTH);
         fenetre.add(panelMilieu, BorderLayout.CENTER);
@@ -53,6 +68,15 @@ public class Affichage implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == jCBRandom) {
+            random = !random;
+            jTFSilence.setEditable(!random);
+        } else {    //bouton demarrer
+            demarrer();
+        }
+    }
+
+    private void demarrer() {
 
     }
 }
